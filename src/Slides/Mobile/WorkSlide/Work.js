b179ff93bedad67f1cb9e1130f8bd5e089e75c25
Prakrut Patel <ppatel@eckerd.edu>
2022-07-23 03:14:23 -0400

    Initial commit

---
+++ src/Slides/Mobile/WorkSlide/Work.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import vhCheck from 'vh-check';
+import TextContent from './TextContent';
+import ImageContent from './ImageContent';
+
+const Container = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    /* border: 1px dashed red; */
+`;
+
+class Work extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vh: 0,
+      slideNumber: 0,
+    };
+    this.pageSplitTimes = 1.3;
+    this.lastScrollTop = 0;
+    this.scrollDirectionDown = true;
+    this.handleScroll = this.handleScroll.bind(this);
+    this.workDetails = [
+      {
+        number: '',
+        projectName: '',
+        projectDesc: '',
+        projectType: '',
+        roles: [''],
+      },
+      {
+        number: '01',
+        projectName: 'Gym Rats',
+        projectDesc: 'Workout app that allows users to create their own workouts and share them. App was created as a part of my senior capstone project.',
+        projectType: 'ANDROID & iOS APP',
+        roles: ['Full Stack Developer', 'UI Designer', 'Product Manager'],
+      },
+      {
+        number: '02',
+        projectName: 'Context RCNN',
+        projectDesc: 'Implement usage of Context RCNN and Faster RCNN models to monitor and conserve wildlife. My efforts included training and deploying the models into a human-in-the-loop continuous learning system.',
+        projectType: 'ML RESEACH',
+        roles: ['Machine Learning Engineer', 'Coder'],
+      },
+      {
+        number: '03',
+        projectName: 'Mobile MakerSpace',
+        projectDesc: 'Responsive user facing web application for creating modular line drawings for CNC and laser cutters',
+        projectType: 'WEB APP',
+        roles: ['Front-end Developer', 'UI Designer'],
+      },
+      {
+        number: '04',
+        projectName: 'Tesla app',
+        projectDesc: 'iOS app concept to control Tesla cars remotely.',
+        projectType: 'iOS APP CONCEPT',
+        roles: ['UI Designer'],
+      },
+      {
+        number: '',
+        projectName: '',
+        projectDesc: '',
+        projectType: '',
+        roles: [''],
+      },
+    ];
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    const vhDiff = vhCheck().offset;
+    this.setState(
+      {
+        vh: Math.round(
+          (window.document.documentElement.clientHeight + vhDiff) * this.pageSplitTimes,
+        ),
+      },
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    const { body, documentElement } = event.srcElement;
+    const { vh, slideNumber } = this.state;
+    const scrollDistance = Math.max(body.scrollTop, documentElement.scrollTop);
+    if (scrollDistance > this.lastScrollTop) {
+      this.scrollDirectionDown = true;
+    } else {
+      this.scrollDirectionDown = false;
+    }
+    this.lastScrollTop = scrollDistance;
+    // console.log(scrollDistance);
+
+    if (Math.floor(scrollDistance / vh) !== slideNumber
+      && slideNumber < this.workDetails.length - 1) {
+      this.setState({ slideNumber: Math.floor(scrollDistance / vh) });
+    } else if (slideNumber === this.workDetails.length - 1
+      && (Math.floor(scrollDistance / vh) < slideNumber)) {
+      this.setState({ slideNumber: Math.floor(scrollDistance / vh) });
+    }
+  }
+
+  changeTextContentBasedOnScroll() {
+    const { slideNumber } = this.state;
+    const refresh = true;
+    return (
+      <TextContent
+        number={this.workDetails[slideNumber].number}
+        projectName={this.workDetails[slideNumber].projectName}
+        projectDesc={this.workDetails[slideNumber].projectDesc}
+        projectType={this.workDetails[slideNumber].projectType}
+        roles={this.workDetails[slideNumber].roles}
+        refreshToggle={refresh}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.changeTextContentBasedOnScroll()}
+        <ImageContent pageSplitTimes={this.pageSplitTimes} />
+      </Container>
+    );
+  }
+}
+
+export default Work;
---
+++ src/Slides/WideScreen/WorkSlide/ParallaxImages/ComingOrNotImages.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import cmgOrNotTabletImg from '../../../../Assets/Images/ComingOrNot/Tablet.png';
+import cmgOrNotIphoneImg from '../../../../Assets/Images/ComingOrNot/Iphone.png';
+import cmgOrNotComputerImg from '../../../../Assets/Images/ComingOrNot/Computer.png';
+
+
+const Iphone = styled.img.attrs({
+  style: ({ scroll }) => ({
+    transform: `translate(0px,-${(scroll) * 2}%)`,
+  }),
+})`
+transition: transform 0.2s ease-out;
+position: absolute;
+bottom:-40vh;
+left: -5vw;
+/* border: 1px dashed red; */
+height: 60vh;
+`;
+
+const Tablet = styled.img.attrs({
+  style: ({ scroll }) => ({
+    transform: `translate(0px,-${(scroll) * 6}%) scale(0.7)`,
+  }),
+})`
+transition: transform 0.2s ease-out;
+position: absolute;
+bottom: -80vh;
+right:-8vw;
+/* border: 1px dashed red; */
+height: 80vh; 
+`;
+const Computer = styled.img.attrs({
+  style: ({ scroll }) => ({
+    transform: `translate(0px,-${(scroll) * 8}%) scale(0.9)`,
+  }),
+})`
+transition: transform 0.2s ease-out;
+position: absolute;
+bottom: -40vh;
+left:0vw;
+/* border: 1px dashed red; */
+height: 80vh; 
+`;
+
+class ComingOrNotImages extends Component {
+  render() {
+    let { scrollPercent } = this.props;
+    const {
+      boxHeight, index, scrollHeight, screenHeight,
+    } = this.props;
+    const heighttoBeReducedinVH = ((boxHeight * index) - 100);
+    const scrollOffset = (screenHeight * heighttoBeReducedinVH) / 100;
+    const scrollOffsetInPercent = (scrollOffset * 100 / scrollHeight) + index - 1;
+    scrollPercent -= scrollOffsetInPercent;
+
+    return (
+      <React.Fragment>
+        <Computer src={cmgOrNotComputerImg} scroll={scrollPercent} alt="cmgOrNotComputer" />
+        <Tablet src={cmgOrNotTabletImg} scroll={scrollPercent} alt="cmgOrNotTablet" />
+        <Iphone src={cmgOrNotIphoneImg} scroll={scrollPercent} alt="cmgOrNotIphone" />
+      </React.Fragment>
+    );
+  }
+}
+
+ComingOrNotImages.propTypes = {
+  boxHeight: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+  screenHeight: PropTypes.number.isRequired,
+  scrollHeight: PropTypes.number.isRequired,
+  scrollPercent: PropTypes.number.isRequired,
+};
+
+export default ComingOrNotImages;
