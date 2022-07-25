23eacc171667f1a1a7394cc96c207739c126a40c
Prakrut Patel <ppatel@eckerd.edu>
2022-07-25 13:17:21 -0400

    GA

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -37715,7 +37715,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50193" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50477" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
